46d4b90b22a3520d1a12d1e70d9db4965bc02206
davy.red.m.navarro <davy.red.m.navarro@accenture.com>
2023-03-20 22:09:54 +0800

    added

--- source_code/backend/nodejs/security/config.js
+++ source_code/backend/nodejs/security/config.js
@@ -1,21 +1,21 @@
 let config = {
     local:{
-        identityMetadata: `https://login.microsoftonline.com/ds.dev.accenture.com/.well-known/openid-configuration`, 
+        identityMetadata: `https://login.microsoftonline.com/<non_prod_tenant>/.well-known/openid-configuration`, 
         clientID: '~~~eso-development-clientid~~~',
         issuer:  "https://login.microsoftonline.com/<non_prod_tenant>/v2.0"
     },
     dev:{
-        identityMetadata: `https://login.microsoftonline.com/ds.dev.accenture.com/.well-known/openid-configuration`, 
+        identityMetadata: `https://login.microsoftonline.com/<non_prod_tenant>/.well-known/openid-configuration`, 
         clientID: '~~~eso-development-clientid~~~',
         issuer:  "https://login.microsoftonline.com/<non_prod_tenant>/v2.0"
     },
     stage:{
-        identityMetadata: `https://login.microsoftonline.com/ds.dev.accenture.com/.well-known/openid-configuration`, 
+        identityMetadata: `https://login.microsoftonline.com/<non_prod_tenant>/.well-known/openid-configuration`, 
         clientID: '~~~eso-staging-clientid~~~',
         issuer:  "https://login.microsoftonline.com/<non_prod_tenant>/v2.0"
     },
     prod:{
-        identityMetadata: `https://login.microsoftonline.com/accenture.com/.well-known/openid-configuration`, 
+        identityMetadata: `https://login.microsoftonline.com/<prod_tenant>/.well-known/openid-configuration`, 
         clientID: '~~~eso-production-clientid~~~',
         issuer:  "https://login.microsoftonline.com/<prod_tenant>/v2.0"
     }
